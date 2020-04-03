58c0827ec87352d55bf0f321b993be75ee433ca8
MannieC <manniec00@gmail.com>
2020-04-03 15:35:56 -0700

    create SHW4.R

---
+++ SHW4.R
@@ -0,0 +1,5 @@
+#In the Spring of 2020, the Covid-19 strain of coronaviruses led to a worldwide pandemic. Today we will practice computing probabilities. The incubation period of a disease is the amount of time between contracting the disease and showing symptoms. Here, I am calling the "illness period" the amount of time between showing symptoms and being ready to interact in public again. For Covid-19, the population statistics are
+#incubation period: mean = 8 days, standard deviation = 3 days
+#illness period: mean = 4 days, standard deviation = 2 days
+#Since there have been thousands of cases worldwide, assume a normal distribution for the calculations.
+
